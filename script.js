6a97fad7f73df70c4e7735c1da6cb48c0e032f42
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-19 22:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function highlight() {
-    //Write your code here
+  // Get all the bold elements in the paragraph
+  var boldElements = document.querySelectorAll("p strong");
 
-
+  // Set the color of each bold element to green
+  for (var i = 0; i < boldElements.length; i++) {
+    boldElements[i].style.color = "green";
+  }
 }
-
 
 function return_normal() {
-    //Write your code here
+  // Get all the bold elements in the paragraph
+  var boldElements = document.querySelectorAll("p strong");
 
-    
+  // Set the color of each bold element back to black
+  for (var i = 0; i < boldElements.length; i++) {
+    boldElements[i].style.color = "black";
+  }
 }
+
+// Add event listeners to the link
+var link = document.getElementById("myLink");
+link.addEventListener("mouseover", highlight);
+link.addEventListener("mouseout", return_normal);
