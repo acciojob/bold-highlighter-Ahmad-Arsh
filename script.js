019916ad3024da65528ea76b8c7a490e3d816783
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-19 22:26:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 function highlight() {
-  // Get all the bold elements in the paragraph
-  var boldElements = document.querySelectorAll("p strong");
-
-  // Set the color of each bold element to green
-  for (var i = 0; i < boldElements.length; i++) {
-    boldElements[i].style.color = "green";
+  // Get all the <strong> elements in the paragraph
+  var boldWords = document.getElementsByTagName("strong");
+  
+  // Loop through all the <strong> elements and set their color to green
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "green";
   }
 }
 
 function return_normal() {
-  // Get all the bold elements in the paragraph
-  var boldElements = document.querySelectorAll("p strong");
-
-  // Set the color of each bold element back to black
-  for (var i = 0; i < boldElements.length; i++) {
-    boldElements[i].style.color = "black";
+  // Get all the <strong> elements in the paragraph
+  var boldWords = document.getElementsByTagName("strong");
+  
+  // Loop through all the <strong> elements and set their color back to black
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "black";
   }
 }
-
-// Add event listeners to the link
-var link = document.getElementById("myLink");
-link.addEventListener("mouseover", highlight);
-link.addEventListener("mouseout", return_normal);
